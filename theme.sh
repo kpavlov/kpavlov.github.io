4583ab9a83c784c14a5fc6e251118b99f4881215
kpavlov <git@konstantinpavlov.net>
2017-06-09 06:33:11 +0300

    buildfile

--- theme.sh
+++ theme.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p themes
-(cd themes && rm -rf hugo-theme-ghostiumx && git clone --depth 1 --branch master --single-branch https://github.com/kpavlov/hugo-theme-ghostiumx.git)
+THEME=hugo-theme-ghostiumx
+(cd themes && rm -rf ${THEME} && git clone --depth 1 --branch master --single-branch https://github.com/kpavlov/${THEME}.git &&  rm -rf ${THEME}/.git)
